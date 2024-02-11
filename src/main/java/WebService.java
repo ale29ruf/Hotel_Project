8e47ea934c160a887daa5b607857e9e12ac150e9
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-11 19:57:56 +0100

    Configuring http request

--- src/main/java/WebService.java
+++ src/main/java/WebService.java
@@ -1,9 +1,26 @@
+import scala.collection.Map;
+
 import static spark.Spark.*;
 
 public class WebService {
     public static void main(String[] args) {
         // Specifica la porta personalizzata (ad esempio, 8080)
         port(8080);
+
+        // Abilita CORS per consentire l'accesso da http://localhost:57479
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
 
         // Definizione del percorso per le chiamate HTTP GET
         get("/hello", (request, response) -> {
@@ -17,7 +34,16 @@ public class WebService {
         });
 
         get("/nationalityScore", (request, response) -> {
-            return NationalityScoreAnalysis.getNationalityScore();
+            System.out.println("Chiamata pervenuta");
+            Map<String, Object> result = NationalityScoreAnalysis.getNationalityScore();
+            System.out.println("Chiamata in invio");
+            return result;
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         });
     }
 }
