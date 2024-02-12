63e44fba1b02d18134ca5557cb16f6e0664f5040
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-12 19:17:08 +0100

    Configuring Function1

--- src/main/java/WebService.java
+++ src/main/java/WebService.java
@@ -5,9 +5,16 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule;
 import static spark.Spark.*;
 
 public class WebService {
+
+
     public static void main(String[] args) {
         // Specifica la porta personalizzata (ad esempio, 8080)
         port(8080);
+
+        // Creazione dell'ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new DefaultScalaModule());
+
         // Abilita CORS per tutte le origini e tutte le richieste
         options("/*", (request, response) -> {
             String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
@@ -23,21 +30,9 @@ public class WebService {
             return "OK";
         });
 
-        // Creazione dell'ObjectMapper
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new DefaultScalaModule());
-
-
 
         // Abilita CORS per tutte le origini
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-
-
-
-        // Definizione del percorso per le chiamate HTTP GET
-        get("/hello", (request, response) -> {
-            return "Hello World!";
-        });
 
         // Definizione del percorso per le chiamate HTTP POST
         post("/hello", (request, response) -> {
@@ -48,6 +43,11 @@ public class WebService {
         get("/nationalityScore", (request, response) -> {
             // Conversione della mappa in JSON
             return mapper.writeValueAsString(NationalityScoreAnalysis.getNationalityScore());
+        });
+
+        get("/Function1", (request, response) -> {
+            // Conversione della mappa in JSON
+            return mapper.writeValueAsString(Function1.eseguiAnalisi());
         });
     }
 }
